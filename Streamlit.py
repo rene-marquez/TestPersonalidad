0e78dad3687a58327452208b3d1721d35a1c1097
Rene Marquez <rene.marquez.m@outlook.com>
2023-05-17 17:34:37 -0600

    creando grafica de línea

--- Streamlit.py
+++ Streamlit.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
 
-st.table(dataframe)
+st.line_chart(chart_data)
