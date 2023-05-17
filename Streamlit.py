f48ae2a07f42e1001cabb780622b7f3c87b815ba
Rene Marquez <rene.marquez.m@outlook.com>
2023-05-17 17:23:42 -0600

    creando dataframe random

--- Streamlit.py
+++ Streamlit.py
@@ -1,8 +1,13 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
+"""
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
+"""
