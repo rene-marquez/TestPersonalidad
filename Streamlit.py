af26c4778ab80611140f571757a262a5bb915d24
Rene Marquez <rene.marquez.m@outlook.com>
2023-05-17 17:38:45 -0600

    creando agregando inserción de texto

--- Streamlit.py
+++ Streamlit.py
@@ -10,3 +10,8 @@ st.map(map_data)
 
 x = st.slider('x')  # 👈 this is a widget
 st.write(x, 'squared is', x * x)
+
+st.text_input("Your name", key="name")
+
+# You can access the value at any point with:
+st.session_state.name
