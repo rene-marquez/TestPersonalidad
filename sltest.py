59e618895f610529dfceeb126d460db9191a2944
Rene Marquez <rene.marquez.m@outlook.com>
2023-05-26 15:02:51 -0600

    realizando prueba de stop

--- sltest.py
+++ sltest.py
@@ -1,31 +1,7 @@
 import streamlit as st
 
-some_list = [1, 2, 3, 4, 5]
-
-if "data" not in st.session_state:
-    st.session_state["data"] = {
-        1: 123,
-        2: 456,
-    }
-
-
-def get_data(key: int):
-    return st.session_state["data"].get(key, None)
-
-
-def add_data(key: int, value: int):
-    st.session_state["data"][key] = value
-
-
-for e in some_list:
-    result = get_data(e)
-    if not result:
-        new_value = st.number_input(f"Missing value for {e}", value=5)
-        tmp_button = st.button("Add value to database", key=f"missing_{e}")
-        if tmp_button:
-            add_data(e, new_value)
-            st.experimental_rerun()
-        else:
-            st.stop()
-
-    st.write(e, "=", st.session_state["data"][e])
+name = st.text_input('Name')
+if not name:
+  st.warning('Please input a name.')
+  st.stop()
+st.success('Thank you for inputting a name.')
