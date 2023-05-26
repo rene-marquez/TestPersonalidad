03c92c21ff7ec8b4118cc79711c9210c3c4f22f2
Rene Marquez <rene.marquez.m@outlook.com>
2023-05-26 12:04:52 -0600

    creando ejemplo para detener en espera de imput

--- sltest.py
+++ sltest.py
@@ -1,113 +1,31 @@
-
 import streamlit as st
-import requests
-import pandas as pd
-import matplotlib.pyplot as plt
-import requests
-import numpy as np
-import seaborn as sns
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse
 
-# Solicitar el nombre por teclado
+some_list = [1, 2, 3, 4, 5]
+
+if "data" not in st.session_state:
+    st.session_state["data"] = {
+        1: 123,
+        2: 456,
+    }
 
 
-st.text_input("¿Cuál es tu nombre?:", key="nombre")
-userName = st.session_state.nombre
-# Solicitar el identificador por teclado
-st.text_input("¿Cuál es el Id del resultado del test de la pagina?:", key="identificador")
-identificador = st.session_state.identificador
-
-while True:
-    # Concatenar el identificador a la URL base del sitio web
-    url = f"https://bigfive-test.com/result/{identificador}"
-
-    # Obtener la respuesta HTTP de la URL
-
-    response = requests.get(url)
-
-    # Buscar la frase ""Request failed with status code 500"" en la URL    
-    if "Request failed with status code 500" in response.text:
-     identificador = st.text_input("La URL no es válida. Por favor, verifica el identificador del resultado del test.")
-    else:
-     # Crear el objeto soup object desde response
-     soup = BeautifulSoup(response.content, "html.parser")
-     break
+def get_data(key: int):
+    return st.session_state["data"].get(key, None)
 
 
-# Definiendo la lista de atributos a extraer
-atributos = ["Neurosis", "Ansiedad", "Ira", "Depresión", "Vergüenza", 
-              "Falta de moderacion", "Vulnerabilidad", "Extroversión", 
-              "Cordialidad", "Sociabilidad", "Confianza", "Nivel de actividad", 
-              "Búsqueda de nuevas experiencias", "Alegría", "Apertura a experiencias", 
-              "Imaginación", "Interes artístico", "Sensibilidad", "Ansias de aventura", 
-              "Intelecto", "Liberalismo", "Simpatía", "Confianza", "Moral", "Altruismo", 
-              "Cooperación", "Modestia", "Empatía", "Meticulosidad", "Autoeficacia", 
-              "Orden", "Sentido del deber", "Orientación a objetivos", "Disciplina", "Prudencia"]
+def add_data(key: int, value: int):
+    st.session_state["data"][key] = value
 
-# Creando una lista para almacenar los resultados
-resultados = [identificador, userName.title()]
 
-# loop over the attributes and extract the corresponding scores
-for atributo in atributos:
-    # find the corresponding div
-    div = soup.find("a", {"href": f"#{atributo.lower()}"})
-    # extract the score
-    score = div.find_next('p').text.split(":")[1].strip()
-    # Validando que el resultado a extraer sean solo dos digitos
-    if score[3] == "-":
-        score = int(score[:2])
-    else:        
-        # Validando que el resultado a extraer sean solo un digito
-        if score[2] == "-":
-            score = int(score[:1])
-        # Validando que el resultado a extraer sean tres digitos
+for e in some_list:
+    result = get_data(e)
+    if not result:
+        new_value = st.number_input(f"Missing value for {e}", value=5)
+        tmp_button = st.button("Add value to database", key=f"missing_{e}")
+        if tmp_button:
+            add_data(e, new_value)
+            st.experimental_rerun()
         else:
-            score = int(score[:3])   
-        
-    # append the attribute name and score to the results list
-    resultados.append(score)    
+            st.stop()
 
-# create a pandas DataFrame from the results list
-
-testPersonalidad = pd.DataFrame([resultados], columns=["Identificador", "Nombre","Neurosis", "Ansiedad", "Ira", "Depresión", "Vergüenza", 
-                                                        "Falta de moderacion", "Vulnerabilidad", "Extroversión", 
-                                                        "Cordialidad", "Sociabilidad", "Confianza", "Nivel de actividad", 
-                                                        "Búsqueda de nuevas experiencias", "Alegría", "Apertura a experiencias", 
-                                                        "Imaginación", "Interes artístico", "Sensibilidad", "Ansias de aventura", 
-                                                        "Intelecto", "Liberalismo", "Simpatía", "Confianza 2", "Moral", "Altruismo", 
-                                                        "Cooperación", "Modestia", "Empatía", "Meticulosidad", "Autoeficacia", 
-                                                        "Orden", "Sentido del deber", "Orientación a objetivos", "Disciplina", "Prudencia"
-                                                         ])
-
-# print the DataFrame
-st.write("Here's our first attempt at using data to create a table:")
-
-st.dataframe(testPersonalidad.style.highlight_max(axis=0))
-"""
-datos5 = testPersonalidad.loc[:, ["Neurosis", "Extroversión", "Apertura a experiencias", "Simpatía", "Meticulosidad"]]
-
-etiquetas=['Neurosis', 'Extroversión', 'Apertura a experiencias', 'Simpatía', 'Meticulosidad']
-
-lista = list(datos5.iloc[0])
-lista=np.concatenate((lista, [lista[0]]))
-esperado = (60, 80, 70, 60, 80, 60)
-
-
-plt.figure(figsize =(10, 6))
-plt.subplot(polar = True)
-
-theta = np.linspace(0, 2 * np.pi, len(lista))
-
-lineas, labels = plt.thetagrids(range(0, 360, int(360/len(etiquetas))),
-                                                         (etiquetas))
-plt.plot(theta, esperado)
-plt.plot(theta, lista)
-plt.fill(theta, lista, 'b', alpha = 0.1)
-
-
-
-plt.legend(labels =("Prospecto", userName.title()), loc = 3, framealpha=1)
-plt.savefig(userName.title() + " test personalidad stats 5.png")
-plt.show()
-"""
+    st.write(e, "=", st.session_state["data"][e])
