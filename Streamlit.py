cd2c0557e8c1bc4bedb4d7961a55f20739168d43
Rene Marquez <rene.marquez.m@outlook.com>
2023-05-30 16:55:17 -0600

    Quitando stop en la captura de nombre

--- Streamlit.py
+++ Streamlit.py
@@ -14,9 +14,9 @@ import psycopg2
 st.text_input("¿Cuál es tu nombre?:", key="nombre")
 userName = st.session_state.nombre
 if not userName:
-  st.warning("Por favor captura un nombre.")
+  #st.warning("Por favor captura un nombre.")
   #st.stop()
-st.success("Gracias.")
+#st.success("Gracias.")
 
 # Solicitar el identificador por teclado
 st.text_input("¿Cuál es el Id del resultado del test de la pagina https://bigfive-test.com/es?:", key="identificador")
